7d6c2813d5889aa218fdee379d2f42a61f9d28c2
Raki <824691+Raki@users.noreply.github.com>
2023-02-25 23:10:20 -0700

    Bit Twiddling

---
+++ BitTwiddling.cpp
@@ -0,0 +1,30 @@
+#include <fmt/format.h>
+#include <vector>
+#include <chrono>
+#include <future>
+
+
+inline bool isPowerof2(uint32_t n)
+{
+	int res = n & (n-1);
+	return (res==0);
+}
+
+inline bool haveOppositSigns(int a,int b)
+{
+	bool res = !((a ^ b) < 0);
+	return res;
+}
+
+int main()
+{
+	/*for (uint32_t n = 1; n <= 2000; n++)
+	{
+		if (isPowerof2(n))
+			fmt::print("{} is power of 2\n", n);
+	}*/
+
+	fmt::print("{} and {} have same signs : {}",-1,-2,haveOppositSigns(-1,2));
+}
+
+
