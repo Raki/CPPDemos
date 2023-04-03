d04253357e7bf83961cdcf0d4fdf802212d20052
Raki <824691+Raki@users.noreply.github.com>
2023-04-03 16:29:42 -0600

    Basic BVH implementation
    Render time reduced from 112ms to 31ms

--- BVH-Step0.cpp
+++ BVH-Step0.cpp
@@ -8,6 +8,7 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include <stb_image_write.h>
 
+#define USE_BVH
 /*
 * Below code is taken from 
 * https://jacco.ompf2.com/2022/04/13/how-to-build-a-bvh-part-1-basics/
@@ -27,6 +28,7 @@ struct Tri
 };
 
 Tri tris[N];
+uint trisIdx[N];
 
 struct Ray
 {
@@ -35,32 +37,22 @@ struct Ray
 	float t = 1e30f;
 };
 
-struct AABB
-{
-    glm::vec3 min;
-    glm::vec3 max;
-};
-
-//Below is inefficient in terms of memory
-//struct BVHNode
-//{
-//    AABB nodeBounds;
-//    BVHNode* left, * right;
-//    bool isLeaf;
-//    std::vector<Tri*> primitives;
-//};
 
 struct BVHNode
 {
     glm::vec3 aabbMin, aabbMax;
-    uint leftChild,rightChild;
-    bool isLeaf;
-    uint firstPrim, primCount;
+    uint leftChild;
+    uint firstTriIndex, triCount;
+    bool isLeaf()
+    {
+        return triCount > 0;
+    }
+    
 };
 
 // For N primitives BVH will max of 2N=1 NODES  
 BVHNode bvhNodes[2 * N - 1];
-uint rootNodeIdx = 1, nodesUsed = 1;
+uint rootNodeIdx = 0, nodesUsed = 1;
 
 #pragma endregion vars
 
@@ -68,9 +60,13 @@ uint rootNodeIdx = 1, nodesUsed = 1;
 void buildBVH();
 void intersectTri(const Tri& tri, Ray& ray);
 void updateNodeBounds(const uint& nodeIdx);
+void subdivide(uint nodeIdx);
+void intersectBVH(Ray &ray,const uint nodeIndex);
+bool intersectAABB(const Ray& ray,const glm::vec3 bMin,const glm::vec3 bMax);
 inline glm::vec3 minOf2(const glm::vec3& v1,const glm::vec3& v2);
 inline glm::vec3 maxOf2(const glm::vec3& v1, const glm::vec3& v2);
 inline void swap(Tri& tri1,Tri& tri2);
+inline void swap(uint& v1, uint& v2);
 #pragma endregion prototypes
 
 #pragma region functions
@@ -80,16 +76,17 @@ void buildBVH()
     for (size_t i=0;i<N;i++)
     {
         tris[i].centroid = (tris[i].v0 + tris[i].v1 + tris[i].v2) * 0.3333f;
+        trisIdx[i] = i;
     }
 
     //assign all triangles to root Node
     BVHNode& root = bvhNodes[rootNodeIdx];
-    root.leftChild = root.rightChild = 0;
-    root.firstPrim = 0; 
-    root.primCount = N;
+    root.leftChild = 0;
+    root.firstTriIndex = 0; 
+    root.triCount = N;
 
     updateNodeBounds(rootNodeIdx);
-    //subdivide(rootNodeIdx);
+    subdivide(rootNodeIdx);
 }
 
 void intersectTri(const Tri &tri,Ray &ray)
@@ -116,16 +113,99 @@ void updateNodeBounds(const uint& nodeIdx)
     node.aabbMin = glm::vec3(1e30f);
     node.aabbMax = glm::vec3(-1e30f);
 
-    for (size_t first=node.firstPrim,i=0;i<node.primCount;i++)
+    for (size_t first=node.firstTriIndex,i=0;i<node.triCount;i++)
     {
-        Tri& leafTri = tris[first + i];
+        auto leafTriIndex = trisIdx[first + i];
+        Tri& leafTri = tris[leafTriIndex];
         node.aabbMin = minOf2(node.aabbMin,leafTri.v0);
         node.aabbMin = minOf2(node.aabbMin, leafTri.v1);
         node.aabbMin = minOf2(node.aabbMin, leafTri.v2);
-        node.aabbMax = minOf2(node.aabbMax, leafTri.v0);
-        node.aabbMax = minOf2(node.aabbMax, leafTri.v1);
-        node.aabbMax = minOf2(node.aabbMax, leafTri.v2);
+        node.aabbMax = maxOf2(node.aabbMax, leafTri.v0);
+        node.aabbMax = maxOf2(node.aabbMax, leafTri.v1);
+        node.aabbMax = maxOf2(node.aabbMax, leafTri.v2);
     }
+}
+
+void subdivide(uint nodeIdx)
+{
+    BVHNode& node = bvhNodes[nodeIdx];
+    //terminate recursion
+    if (node.triCount <= 2)
+        return;
+    
+    //split plane axis and position
+    auto extent = node.aabbMax - node.aabbMin;
+    int axis = 0;
+    if (extent.y > extent.x) axis = 1;
+    if (extent.z > extent[axis]) axis = 2;
+
+    float splitPos = node.aabbMin[axis] + extent[axis] * 0.5f;
+
+    int i = node.firstTriIndex;
+    int j = i+node.triCount-1;
+
+    while (i <= j)
+    {
+        if (tris[trisIdx[i]].centroid[axis] < splitPos)
+        {
+            i++;
+        }
+        else
+        {
+            swap(trisIdx[i],trisIdx[j--]);
+        }
+    }
+
+    int leftCount = i - node.firstTriIndex;
+    if (leftCount == 0 || leftCount == node.triCount) return;
+    // create child nodes
+    int leftChildIdx = nodesUsed++;
+    int rightChildIdx = nodesUsed++;
+
+    bvhNodes[leftChildIdx].firstTriIndex= node.firstTriIndex;
+    bvhNodes[leftChildIdx].triCount = leftCount;
+
+    bvhNodes[rightChildIdx].firstTriIndex= i;
+    bvhNodes[rightChildIdx].triCount= node.triCount- leftCount;
+    node.triCount = 0;
+    node.leftChild = leftChildIdx;
+
+    updateNodeBounds(leftChildIdx);
+    updateNodeBounds(rightChildIdx);
+
+    //recurse
+    subdivide(leftChildIdx);
+    subdivide(rightChildIdx);
+}
+
+void intersectBVH(Ray& ray, const uint nodeIndex)
+{
+    BVHNode& node = bvhNodes[nodeIndex];
+    if (!intersectAABB(ray, node.aabbMin, node.aabbMax)) return;
+
+    if (node.isLeaf())
+    {
+        for (size_t i = 0; i < node.triCount; i++)
+        {
+            intersectTri(tris[trisIdx[node.firstTriIndex + i]], ray);
+        }
+    }
+    else
+    {
+        intersectBVH(ray, node.leftChild);
+        intersectBVH(ray, node.leftChild+1);
+    }
+}
+
+bool intersectAABB(const Ray& ray, const glm::vec3 bMin, const glm::vec3 bMax)
+{
+    float tx1 = (bMin.x - ray.O.x) / ray.D.x, tx2 = (bMax.x - ray.O.x) / ray.D.x;
+    float tmin = std::min(tx1, tx2), tmax = std::max(tx1, tx2);
+    float ty1 = (bMin.y - ray.O.y) / ray.D.y, ty2 = (bMax.y - ray.O.y) / ray.D.y;
+    tmin = std::max(tmin, std::min(ty1, ty2)), tmax = std::min(tmax, std::max(ty1, ty2));
+    float tz1 = (bMin.z - ray.O.z) / ray.D.z, tz2 = (bMax.z - ray.O.z) / ray.D.z;
+    tmin = std::max(tmin, std::min(tz1, tz2)), tmax = std::min(tmax, std::max(tz1, tz2));
+    return tmax >= tmin && tmin < ray.t&& tmax > 0;
 }
 
 inline glm::vec3 minOf2(const glm::vec3& v1, const glm::vec3& v2)
@@ -146,6 +226,16 @@ inline glm::vec3 maxOf2(const glm::vec3& v1, const glm::vec3& v2)
 
 inline void swap(Tri& tri1, Tri& tri2)
 {
+    Tri temp = tri1;
+    tri1 = tri2;
+    tri2 = temp;
+}
+
+inline void swap(uint& v1, uint& v2)
+{
+    auto temp = v1;
+    v1 = v2;
+    v2 = temp;
 }
 
 #pragma endregion functions
@@ -164,12 +254,10 @@ int main()
 	
 
     //Creat BVH
+    buildBVH();
     
-    
 
-
-
-    constexpr glm::vec3 CAMPOS = glm::vec3(0,0,25);
+    constexpr glm::vec3 CAMPOS = glm::vec3(0,0,18);
     constexpr glm::vec3 TopLeft = glm::vec3(-1, 1, 15);
     constexpr glm::vec3 TopRight = glm::vec3(1, 1, 15);
     constexpr glm::vec3 BottomLeft = glm::vec3(-1, -1, 15);
@@ -180,6 +268,7 @@ int main()
     glm::vec3 triColor = glm::vec3(255*0.1,255*0.2,255*0.3);
     Ray ray;
     ray.O = CAMPOS;
+    auto bgn = std::chrono::high_resolution_clock::now();
     for (int y = 0;y<SHEIGHT;y++)
     {
         for (int x = 0; x < SWIDTH; x++)
@@ -190,6 +279,17 @@ int main()
             ray.D = glm::normalize(pixelPos-CAMPOS);
             ray.t = 1e30f;
             size_t pixelIndex = (y * static_cast<size_t>(SWIDTH) * 3) + static_cast<size_t>(x) * 3;
+           
+#ifdef USE_BVH
+            intersectBVH(ray, rootNodeIdx);
+            if (ray.t < 1e30f)
+            {
+                pixels.at(pixelIndex) = static_cast<unsigned char>(triColor.r);
+                pixels.at(pixelIndex + 1) = static_cast<unsigned char>(triColor.g);
+                pixels.at(pixelIndex + 2) = static_cast<unsigned char>(triColor.b);
+            }
+#else
+
             for (int i = 0; i < N; i++)
             {
                 intersectTri(tris[i], ray);
@@ -200,9 +300,12 @@ int main()
                     pixels.at(pixelIndex+2) = static_cast<unsigned char>(triColor.b);
                 }
             }
+#endif
         }
-    }
 
+    }
+    auto end = std::chrono::high_resolution_clock::now();
+    fmt::print("Time taken {} ms\n",std::chrono::duration_cast<std::chrono::milliseconds>(end-bgn).count());
     stbi_write_jpg("render.jpg", static_cast<int>(SWIDTH), static_cast<int>(SHEIGHT), 3, pixels.data(), 100);
 
 	return EXIT_SUCCESS;
