e4ba254e61f7837ff34bc32e30efc5a86e17f276
Raki <824691+Raki@users.noreply.github.com>
2023-04-11 18:09:56 -0600

    Update Sorting.cpp

--- Sorting.cpp
+++ Sorting.cpp
@@ -12,42 +12,54 @@ inline void swap(T& v1, T& v2)
 	v2 = temp;
 }
 
-size_t partitionMidPoint(std::vector<int> &data,int low,int high)
-{
-	auto pivotData = data.at((low+high)/2);
 
-	while (low<=high)
-	{
-		while (data.at(low) < pivotData)
-			low++;
-		while (data.at(high) > pivotData)
-			high--;
-		if (low <= high)
-		{
-			swap<int>(data.at(low),data.at(high));
-			low++;
-			high--;
-		}
-	}
-
-	return low;
-}
-
-void sort(std::vector<int> &data,const std::vector<int>::size_type fst,const std::vector<int>::size_type lst)
+void quicksort(std::vector<int> &data,int fst,int lst)
 {
 	if (fst < lst)
 	{
-		auto pivot = partitionMidPoint(data, fst, lst);
-		sort(data, fst, pivot - 1);
-		sort(data, pivot + 1,lst);
+		int lIndex, rIndex;
+
+		lIndex = fst;
+		rIndex = lst;
+		auto pivotData = data.at(
+			lIndex + (rIndex - lIndex) / 2
+		);
+
+		while (lIndex <= rIndex)
+		{
+			while (data.at(lIndex) < pivotData)
+				lIndex++;
+			while (data.at(rIndex) > pivotData)
+				rIndex--;
+			if (lIndex <= rIndex)
+			{
+				swap<int>(data.at(lIndex), data.at(rIndex));
+				lIndex++;
+				rIndex--;
+			}
+		}
+
+		if(fst<rIndex)quicksort(data, fst, rIndex);
+		if(lIndex<lst)quicksort(data, lIndex, lst);
 	}
 }
 
+
+
 int main()
 {
-	std::vector<int> arr = {1,7,9,8,5,6,2,3,4};
-
-	sort(arr,0,arr.size()-1);
+	auto beginTime = std::chrono::system_clock::now();
+	decltype(beginTime) endTime;
+	std::vector<int> arr = {8,7,9,4,5,6,2,3,1};
+	arr.clear();
+	for (auto i = 0; i < 2000000; i++)
+	{
+		arr.push_back(rand());
+	}
+	beginTime = std::chrono::system_clock::now();
+	quicksort(arr,0,arr.size()-1);
+	endTime = std::chrono::system_clock::now();
+	fmt::print("qsort took {}ms\n",std::chrono::duration_cast<std::chrono::milliseconds>(endTime - beginTime).count());
 	
 	return EXIT_SUCCESS;
 }
